65fb344d6e9c46fdd4cf62ad8150d38f947048e3
Duojunrui <duojunrui@163.com>
2019-03-23 19:04:39 +0800

    项目环境搭建

---
+++ src/main/java/cn/duojunrui/etims/EtimsApplication.java
@@ -0,0 +1,13 @@
+package cn.duojunrui.etims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtimsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtimsApplication.class, args);
+    }
+
+}
