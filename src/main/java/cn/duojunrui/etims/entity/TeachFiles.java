71ae267bf91f730032234e0b24547a0a4854b4ec
Duojunrui <duojunrui@163.com>
2019-03-23 20:14:19 +0800

    项目环境搭建

---
+++ src/main/java/cn/duojunrui/etims/entity/TeachFiles.java
@@ -0,0 +1,20 @@
+package cn.duojunrui.etims.entity;
+
+/* *
+ * 教学资料实体
+ * 课件、试题、设备知识、项目资料等
+ */
+public class TeachFiles {
+
+    private String id;
+    private String file_name;
+    private String course_type;
+    private String category;
+    private String uploader;
+    private String file_map;
+    private long create_time;
+    private long update_time;
+    private int validity;
+
+
+}
