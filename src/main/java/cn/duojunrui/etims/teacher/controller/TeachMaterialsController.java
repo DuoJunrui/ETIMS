4340eeb2f2051146e0db944f44e92f816264779a
Duojunrui <duojunrui@163.com>
2019-03-23 19:32:14 +0800

    项目环境搭建

---
+++ src/main/java/cn/duojunrui/etims/teacher/controller/TeachMaterialsController.java
@@ -0,0 +1,15 @@
+package cn.duojunrui.etims.teacher.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeachMaterialsController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String searchMaterials(){
+        return "Hello SpringBoot!";
+    }
+
+}
