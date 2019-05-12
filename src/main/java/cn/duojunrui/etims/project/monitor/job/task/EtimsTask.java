7cf018246701bed3b58764ed5f4dd7ae066574c2
Duojunrui <duojunrui@163.com>
2019-05-12 13:06:38 +0800

    定时任务

--- src/main/java/cn/duojunrui/etims/project/monitor/job/task/EtimsTask.java
+++ src/main/java/cn/duojunrui/etims/project/monitor/job/task/EtimsTask.java
@@ -5,18 +5,18 @@ import org.springframework.stereotype.Component;
 /**
  * 定时任务调度测试
  *
- * @className: RyTask
+ * @className: EtimsTask
  * @author: Duojunrui
  * @date: 2019-04-19 20:17
  */
 
-@Component("ryTask")
-public class RyTask {
-    public void ryParams(String params) {
+@Component("etimsTask")
+public class EtimsTask {
+    public void etimsParams(String params) {
         System.out.println("执行有参方法：" + params);
     }
 
-    public void ryNoParams() {
+    public void etimsNoParams() {
         System.out.println("执行无参方法");
     }
 }
